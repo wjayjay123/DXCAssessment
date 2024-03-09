f5aa222c388e4bfcf4bd1e42f3579c259e39154a
Wong Jia Jun <77979635+wjayjay123@users.noreply.github.com>
2024-03-09 23:40:16 +0800

    Create Encoder.java

---
+++ Encoder/Encoder.java
@@ -0,0 +1,35 @@
+package Encoder;
+
+class Encoder extends ReferenceTable {
+
+    String plainText = "";
+    char offSetChar = ' ';
+
+    public String encode(String plainText, char offSetChar) {
+        this.plainText = plainText;
+        this.offSetChar = offSetChar;
+
+        // Get Index of Char from reference table to use as offset
+        Integer offSetIndex = getIndex(offSetChar);
+
+        // Initialize StringBuilder
+        StringBuilder encodedText = new StringBuilder();
+        encodedText.append(offSetChar);
+        for (char chars : plainText.toCharArray()) {
+
+            // Check if Character exist within reference table
+            if (getIndex(chars) != -1) {
+                Integer index = getIndex(chars);
+                // Calculate offset index
+                index = (index - offSetIndex + getLength()) % getLength();
+                // Append character to encoded string
+                encodedText.append(getChar(index));
+            } else if (chars == ' ') {
+                // Append space
+                encodedText.append(' ');
+            }
+        }
+        return encodedText.toString();
+
+    }
+}
