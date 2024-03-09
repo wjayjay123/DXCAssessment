7ec285d620a90251471bd591501d0b91240ad7c5
Wong Jia Jun <77979635+wjayjay123@users.noreply.github.com>
2024-03-10 00:55:11 +0800

    Final clean up and testing

--- Encoder/Main.java
+++ Encoder/Main.java
@@ -1,7 +1,6 @@
 package Encoder;
 
 import java.util.Scanner;
-import Encoder.Encoder;
 
 public class Main {
 
@@ -14,10 +13,19 @@ public class Main {
         System.out.print("Select Option: ");
     }
 
+    private void getDecodeMenu() {
+        System.out.println("--Decode previously encoded text?--");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        System.out.println("3. Back");
+        System.out.print("Select Option: ");
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         Encoder encoder = new Encoder();
+        Decoder decoder = new Decoder();
         String offSetError = "Invalid offset. Please try again\n";
 
         whileloop:
@@ -25,13 +33,12 @@ public class Main {
         while (true) {
             main.getMenu();
             String option = scanner.nextLine();
-
             switch (option.toLowerCase()) {
                 case "encode":
                 case "1":
-                    System.out.println("Enter PlainText: ");
+                    System.out.println("Enter plain text: ");
                     String plainText = scanner.nextLine().toUpperCase();
-                    System.out.println("Enter OffSet: ");
+                    System.out.println("Enter offset: ");
                     String input = scanner.nextLine().toUpperCase();
                     try {
                         char offSet = input.charAt(0);
@@ -47,6 +54,50 @@ public class Main {
                     }
                     break;
 
+                case "decode":
+                case "2":
+                    main.getDecodeMenu();
+                    option = scanner.nextLine();
+                    switch (option.toLowerCase()) {
+                        case "yes":
+                        case "1":
+                            if (encoder.getPrevEncoded() == "") {
+                                System.out.println("No previous encoded text. Please encode a text.\n");
+                                break;
+                            } else {
+                                System.out.println("Prevous encoded text is: " + encoder.getPrevEncoded());
+                                String decodedText = decoder.decode(encoder.getPrevEncoded());
+                                System.out.println("\nThe decoded text is: " + decodedText + "\n");
+                            }
+                            break;
+
+                        case "no":
+                        case "2":
+                            System.out.println("Enter encoded text: ");
+                            String encodedText = scanner.nextLine().toUpperCase();
+                            try {
+                                String decodedText = decoder.decode(encodedText);
+                                if (decodedText != offSetError) {
+                                    System.out.println("\nThe decoded text is: " + decodedText + "\n");
+                                } else {
+                                    System.out.println(decodedText + "\n");
+                                }
+                            } catch (Exception e) {
+                                System.out.println(e);
+                                System.out.println("Invalid input. Please check again\n");
+                            }
+                            break;
+
+                        case "exit":
+                        case "3":
+                            break;
+                        default:
+                            System.out.println("Invalid Option\n");
+                            break;
+                    }
+                    break;
+
+                case "exit":
                 case "3":
                     System.out.println("Exiting program.\n");
                     break whileloop;
--- Encoder/ReferenceTable.java
+++ Encoder/ReferenceTable.java
@@ -1,18 +1,21 @@
 package Encoder;
 
-public class ReferenceTable {
+public abstract class ReferenceTable {
 
     // Create reference table
     private String referenceTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*+,-./";
 
+    // Retrieve index of character input
     protected Integer getIndex(char character) {
         return referenceTable.indexOf(character);
     }
 
+    // Retrieve character using index
     protected char getChar(Integer index) {
         return referenceTable.charAt(index);
     }
 
+    // Retrieve the length of the reference table
     protected Integer getLength() {
         return referenceTable.length();
     }
