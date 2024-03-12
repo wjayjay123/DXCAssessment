d07e91fb22367cd290edea4e178a91585b3d7385
Wong Jia Jun <77979635+wjayjay123@users.noreply.github.com>
2024-03-12 14:06:19 +0800

    Added image to restricted page

--- LoginApp/LoginappFrontend/loginapp-frontend/src/pages/Restricted.jsx
+++ LoginApp/LoginappFrontend/loginapp-frontend/src/pages/Restricted.jsx
@@ -1,8 +1,17 @@
+import { Typography } from "@mui/material";
+import Image from "../image/topsecret.png";
+
 export const Restricted = () => {
   return (
     <div className="App">
       <div className="auth-form-container">
         <label className="header-label">Restricted Page</label>
+        <Typography
+          style={{ color: "black", fontSize: "big", textAlign: "center" }}
+        >
+          Hi Manager, this is a restricted page.
+        </Typography>
+        <img src={Image} height="200px" width="200px" />
       </div>
     </div>
   );
